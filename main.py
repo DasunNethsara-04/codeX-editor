efed388d89664503d4b5b2f1c1e0deb27c1d8bb6
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-04-29 23:09:50 +0530

    dynamic language indicator | shortcuts implemented

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import customtkinter
-from CTkMenuBar import *
+from CTkMenuBar import CTkMenuBar, CustomDropdownMenu
 from tkinter import filedialog
-from utils import file_types
+from utils import file_types, check_file_type
 from customtkinter import CTk, CTkTextbox, CTkLabel, CTkFrame, CTkButton, StringVar, BOTH, BOTTOM, X, RIGHT, LEFT
 
 # required functions
 def clear_code_area() -> None:
     code_area.delete("0.0", "end")
 
-def create_new_file() -> None:
+def create_new_file(event=None) -> None:
+    language_var.set('Text File')
     clear_code_area()
 
-def open_file_content() -> list[str]:
+def open_file_content(event=None) -> None:
     filename: str = filedialog.askopenfilename()
-    content: list[str] = []
+    language_var.set(check_file_type(filename))
     with open(filename, "r") as file:
-        content = [line for line in file.readlines()]
-    return content
-
-def set_content() -> None:
-    file_content:list[str] = open_file_content()
-    clear_code_area()
-    for line in file_content:
-        code_area.insert("0.0", line)
+        code_area.delete("0.0", "end")
+        code_area.insert("0.0", file.read())
 
 def get_code_area_content() -> str:
     return code_area.get("0.0", "end")
 
-def save_file() -> None:
+def save_file(event=None) -> None:
     content: str = get_code_area_content()
     filepath = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=file_types)
     if filepath:
@@ -39,6 +34,21 @@ def save_file_as() -> None:
 
 def change_appearance_mode(mode:str="Dark") -> None:
     customtkinter.set_appearance_mode(mode)
+
+def zoom_in_text(event=None) -> None:
+    global font_size
+    font_size += 2
+    code_area.configure(font=("Consolas", font_size))
+
+def zoom_out_text(event=None) -> None:
+    global font_size
+    font_size -= 2
+    code_area.configure(font=("Consolas", font_size))
+
+def reset_zoom_text(event=None) -> None:
+    global font_size
+    font_size = 13
+    code_area.configure(font=("Consolas", font_size))
 
 # main user interface configurations
 window: CTk = CTk()
@@ -52,6 +62,8 @@ status_var.set("Ready!")
 
 language_var:StringVar = StringVar()
 language_var.set("Text File")
+
+font_size = 13
 
 # user interface widgets
 
@@ -68,7 +80,7 @@ file_dropdown:CustomDropdownMenu = CustomDropdownMenu(widget=file_menu)
 file_dropdown.add_option(option="New File", command=create_new_file)
 file_dropdown.add_option(option="New Project")
 file_dropdown.add_separator()
-file_dropdown.add_option(option="Open File", command=set_content)
+file_dropdown.add_option(option="Open File", command=open_file_content)
 file_dropdown.add_separator()
 file_dropdown.add_option(option="Save File", command=save_file)
 file_dropdown.add_option(option="Save File As...", command=save_file)
@@ -83,7 +95,7 @@ appearance_submenu.add_option("DARK", command=lambda:change_appearance_mode("Dar
 appearance_submenu.add_option("SYSTEM", command=lambda:change_appearance_mode("System"))
 
 # text box
-code_area: CTkTextbox = CTkTextbox(window, font=("Consolas", 25))
+code_area: CTkTextbox = CTkTextbox(window, font=("Consolas", font_size))
 code_area.pack(fill=BOTH, expand=True)
 
 # Now create the status bar
@@ -98,6 +110,20 @@ status_label.pack(side=LEFT, padx=10)
 language_label:CTkLabel = CTkLabel(status_bar, textvariable=language_var, anchor="e")
 language_label.pack(side=RIGHT, padx=10)
 
+# keyboard shortcuts
 
+# zoom
+window.bind("<Control-plus>", zoom_in_text)
+window.bind("<Control-minus>", zoom_out_text)
+window.bind("<Control-0>", reset_zoom_text)
+
+# save file
+window.bind("<Control-s>", save_file)
+
+# open file
+window.bind("<Control-o>", open_file_content)
+
+# new file
+window.bind("<Control-n>", create_new_file)
 
 window.mainloop()
