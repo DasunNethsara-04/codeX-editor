326e6209acc83ec39c213b7e2fc8bb5165089cda
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-05-01 17:37:17 +0530

    New files types added

--- utils/file_types.py
+++ utils/file_types.py
@@ -1,7 +1,12 @@
 file_types: list[tuple[str, str]] = [
-                ("Text File", "*.txt"),
-                ("Python File", "*.py"),
-                ("HTML File", "*.html"),
-                ("CSS File", "*.css"),
-                ("JavaScript File", "*.js"),
-            ]
+    ("Text File", "*.txt"),
+    ("Python File", "*.py"),
+    ("HTML File", "*.html"),
+    ("CSS File", "*.css"),
+    ("JavaScript File", "*.js"),
+    ("Markdown File", "*.md"),
+    ("TypeScript File", "*.ts"),
+    ("TypeScript with JSX", "*.tsx"),
+    ("JavaScript with XML", "*.jsx"),
+    ("JSON File", "*.json"),
+]
