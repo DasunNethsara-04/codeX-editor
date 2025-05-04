b07c918c49187770c02ddebaeb98f80dd7556220
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-05-04 19:32:11 +0530

    keymaps added

---
+++ utils/keymap.py
@@ -0,0 +1,27 @@
+file_shortcuts: list[tuple[str, str]] = [
+    ("New File", "Ctrl + N"),
+    ("Open File", "Ctrl + O"),
+    ("Save File", "Ctrl + S"),
+    ("New Project", "Ctrl + Shift + N"),
+]
+
+edit_shortcuts: list[tuple[str, str]] = [
+    ("Cut", "Ctrl + X"),
+    ("Copy", "Ctrl + C"),
+    ("Paste", "Ctrl + V"),
+    ("Select All", "Ctrl + A"),
+    ("Undo", "Ctrl + Z"),
+    ("Redo", "Ctrl + Y"),
+]
+
+view_shortcuts: list[tuple[str, str]] = [
+    ("Zoom In", "Ctrl+ +"),
+    ("Zoom Out", "Ctrl+ -"),
+    ("Reset Zoom", "Ctrl+ 0"),
+    ("Toggle Sidebar", "Ctrl+ B"),
+]
+
+help_shortcuts: list[tuple[str, str]] = [
+    ("Keyboard Shortcuts", "Ctrl + K"),
+    ("About CodeX", "F1"),
+]
