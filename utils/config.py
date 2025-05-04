29a6274f45927f3bd4a527dc6409ef10bea27cbe
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-05-04 19:32:31 +0530

    application details moved to config.py

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+# APPLICATION DETAILS
+APP_NAME: Final[str] = "CodeX Code Editor"
+APP_VERSION: Final[float] = 1.0
+APP_EDITION: Final[str] = "Stable"
+
+# EDITOR CONFIGS
+font_size: int = 13
