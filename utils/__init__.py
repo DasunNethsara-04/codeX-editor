0962f137864c5218c716a4cc46905c80856b4388
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-05-12 20:41:58 +0530

    all the new modules initialized for import

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,5 @@ from .config import *
 from .file_types import file_types
 from .util_function import check_file_type, create_project_files
 from .change_log import CHANGELOG
+from .theming import SYNTAX_COLORS
+from .syntax_highlight import get_lexer_for_language
