c8257be37f4aa591967b85717b1dffca55a11f9e
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-05-29 17:17:42 +0530

    old files removed

--- utils/config.py
+++
@@ -1,9 +0,0 @@
-from typing import Final
-
-# APPLICATION DETAILS
-APP_NAME: Final[str] = "CodeX Code Editor"
-APP_VERSION: Final[float] = 1.2
-APP_EDITION: Final[str] = "Stable"
-
-# EDITOR CONFIGS
-font_size: int = 13
